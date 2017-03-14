ab5ee206fe2a44e548e443879b55aeba7acb86ab
lmwchen <634401598@qq.com>
2017-03-14 22:10:04 +0800

    添加链接

--- js/js1.js
+++ js/js1.js
@@ -2,20 +2,20 @@
 $('.mask1').mouseover(function () {
     $('.mask1').animate({
          opacity:'0'
-     });
+     },"slow");
 });
 $('.mask1').mouseout(function () {
     $('.mask1').animate({
         opacity:'0.6'
-    });
+    },"slow");
 });
 $('.mask2').mouseover(function () {
     $('.mask2').animate({
         opacity:'0'
-    });
+    },"slow");
 });
 $('.mask2').mouseout(function () {
     $('.mask2').animate({
         opacity:'0.6'
-    });
+    },"slow");
 });
