8e19d72c915cf262752c7254c6a93090325b4517
lmwchen <634401598@qq.com>
2017-03-14 21:58:24 +0800

    动画效果

---
+++ js/js1.js
@@ -0,0 +1,21 @@
+// 主页
+$('.mask1').mouseover(function () {
+    $('.mask1').animate({
+         opacity:'0'
+     });
+});
+$('.mask1').mouseout(function () {
+    $('.mask1').animate({
+        opacity:'0.6'
+    });
+});
+$('.mask2').mouseover(function () {
+    $('.mask2').animate({
+        opacity:'0'
+    });
+});
+$('.mask2').mouseout(function () {
+    $('.mask2').animate({
+        opacity:'0.6'
+    });
+});
